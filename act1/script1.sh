c010de3d0340633bc4656f67dfdeb25ec1467391
shah-tech <shahjanali@yahoo.com>
2024-02-05 10:10:50 +0000

    added 1 and 8

---
+++ act1/script1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#Author: Shahjan
+#
+#Script purpose:Print their Username,current date,current time,current directory
+#How many files are in the directory & whats the largest file in the directory
+#
+#Script Usage: ./script1.sh
+
+date=$(date +%d/%m/%Y)
+echo $date
+echo $USER
+echo $PWD
---
+++ act8/script8.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+#Author: shahjan
+#
+#Script Purpose:
+#
+#Script usage:
+#
+#
+take_one=read line
+
+if( int($take_one) == $take_one)
+	echo "Good input"
+
+else: 
